e6419a75387d8623f56362a43612e8570dfaae0f
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2015-12-28 21:50:51 +0530

    Fixing according to submodules dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from app.quora import views
-
-app.register_blueprint(views.mod)
+if __name__ == '__main__':
+    from flask import Flask
+    app = Flask(__name__)
+    from app.quora import views
+    app.register_blueprint(views.mod)
--- app/quora/views.py
+++ app/quora/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request
 
-mod = Blueprint('simple_page', __name__, template_folder='templates')
+mod = Blueprint('quora', __name__, template_folder='templates')
 
 @mod.route('/', methods=["GET"])
 def index():
-    return render_template('index.html')
+    return render_template('quora_widget_base.html')
